33c1b62eef913643998457d766f3a6e13d5b7d7d
carlosf1809 <carlosmoreiraaraujo18@gmail.com>
2020-10-19 22:12:50 -0300

    up

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Title} from './styles'
+
+const Dashboard: React.FC = () => {
+    return (
+        <Title>
+            Explore repositório no github
+        </Title>
+    )
+};
+
+export default Dashboard;
